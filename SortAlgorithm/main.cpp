65a7635cafb07e4cd64c8e152c70eb9e09626e8f
altangerel tumuruu <aagii.tumuruu@yahoo.com>
2016-04-29 14:31:01 +0900

    Initial Commit

---
+++ SortAlgorithm/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SortAlgorithm
+//
+//  Created by Tesla on 2016-04-29.
+//  Copyright © 2016 Tesla. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
